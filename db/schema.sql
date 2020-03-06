51270d9f35d8dfb8305e239302e9a57a1ef43e19
aagrosse <aagrock81@yahoo.com>
2020-03-06 12:04:13 -0500

    added schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,5 +6,5 @@ USE burgers_db;
 CREATE TABLE burgers(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     burger_name VARCHAR(30) NOT NULL,
-    devoured boolean NOT NULL
+    devoured boolean NOT NULL default 0
 );
