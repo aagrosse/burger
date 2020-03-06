8e780c205ecebe4ce1ee6c68a778635c54ec6b87
aagrosse <aagrock81@yahoo.com>
2020-03-06 12:23:01 -0500

    updated connection params

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
         port: 3306,
         host: 'localhost',
         user: 'root',
-        password: 'Abc!1234', //process.env.MYSQL_PASSWORD
+        password: process.env.MYSQL_PASSWORD
         database: 'burgers_db'
     });
 }
