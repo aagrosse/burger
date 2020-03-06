bb022bdc950dcc813522f5bd3d6b650bf968c80f
aagrosse <aagrock81@yahoo.com>
2020-03-06 12:09:52 -0500

    added orm file

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,34 @@
 const connection = require('./connection.js');
 
 const orm = {
-    selectAll: function(){
-
+    selectAll: function (cb) {
+        const query = 'SELECT * FROM burgers;';
+        connection.query(query, (err, result) => {
+            if (err) throw err;
+            cb(result);
+        });
     },
-
-    insertOne: function(){
-        
+    insertOne: function (burger, cb) {
+        const query = `INSERT INTO burgers (burger_name) VALUES ("${burger}")`;
+        connection.query(query, (err, result) => {
+            if (err) throw err;
+            cb(result);
+        });
     },
-
-    updateOne: function(){
-        
+    updateOne: function (burgerID, cb) {
+        const query = `UPDATE burgers SET devoured = 1 WHERE ID = ${burgerID}`;
+        connection.query(query, (err, result) => {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    deleteOne: function(table, condition, cb) {
+        var query = "DELETE FROM "+ table + " WHERE "+ condition;
+        console.log(query)
+        connection.query(query, condition, function(err, results) {
+            if (err) throw err;
+            cb(results)
+        })
     }
 }
 
